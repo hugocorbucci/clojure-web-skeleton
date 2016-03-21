c9ddd0ab932fb8bbf7634b355c2099cd1a7e0ad7
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-03-21 16:19:25 -0300

    Upgrading gaffer to allow for .env

--- dev.sh
+++ dev.sh
@@ -5,15 +5,20 @@ set -o
 ./setup.sh
 (mongod --version &> /dev/null) || ( (brew --version &> /dev/null) && (brew install mongodb > /dev/null) )
 (mongod --version &> /dev/null) || ( (apt-get --version &> /dev/null) && (apt-get install -y mongodb-org > /dev/null) )
-GAFFER_VERSION='gaffer-0.0.1'
-if [ ! -f ./bin/gaffer ]; then
-  ( (curl --silent -L "https://github.com/jingweno/gaffer/releases/download/0.0.1/${GAFFER_VERSION}-dist.zip" > tmp/gaffer.zip) &&
-    (unzip -d . -n tmp/gaffer.zip) && (mv ${GAFFER_VERSION}/bin/* ./bin) && (mv ${GAFFER_VERSION}/lib/* ./lib) && (rm -Rf ${GAFFER_VERSION}) )
+
+PROVIDER='hugocorbucci' # Original provider: 'jingweno'
+GAFFER_VERSION='0.0.2'
+if [[ ! -f ./bin/gaffer || ${GAFFER_VERSION} != `./bin/gaffer version` ]]; then
+  ( (curl --silent -L "https://github.com/${PROVIDER}/gaffer/releases/download/${GAFFER_VERSION}/gaffer-${GAFFER_VERSION}-dist.zip" > tmp/gaffer.zip) &&
+    (unzip -d . -n tmp/gaffer.zip) && (mv gaffer-${GAFFER_VERSION}/bin/* ./bin) && (mv gaffer-${GAFFER_VERSION}/lib/* ./lib) && (rm -Rf gaffer-${GAFFER_VERSION}) )
 fi
+
 ./bin/lein deps
+
 mkdir -p tmp/db/data
 ps xau | grep livereload | grep -v grep | awk '{print $2}' | xargs kill
-APP_NAME=skeleton
+
+APP_NAME='skeleton'
 if [ ! -f ./.env ]; then
   printf "DATABASE_URL=mongodb://localhost/${APP_NAME}\n" > ./.env
 fi
