50a9c99f3c5c4da07424c12b9e27f11b892e99ee
Hugo Corbucci <hugo.corbucci@gmail.com>
2017-08-27 22:26:35 -0400

    Upgrading dependencies

--- project.clj
+++ project.clj
@@ -12,25 +12,25 @@
       [org.clojure/data.zip "0.1.2"]
       [org.clojure/data.xml "0.0.8"]
       [org.clojure/data.json "0.2.6"]
-      [org.clojure/tools.logging "0.3.1"]
-      [javax.servlet/javax.servlet-api "3.1.0"]
-      [compojure "1.5.1"]
+      [org.clojure/tools.logging "0.4.0"]
+      [javax.servlet/javax.servlet-api "4.0.0"]
+      [compojure "1.6.0"]
       [ring/ring-defaults "0.2.1"]
-      [ring/ring-core "1.5.0"]
-      [ring/ring-devel "1.5.0"]
+      [ring/ring-core "1.6.2"]
+      [ring/ring-devel "1.6.2"]
       [ring/ring-json "0.4.0"]
       [ring.middleware.logger "0.5.0"]
       [http-kit "2.2.0"]
       [environ "1.1.0"]
-      [clj-http "3.4.1"]
+      [clj-http "3.7.0"]
       [hiccup "1.0.5"]
       [com.novemberain/monger "3.1.0"]
-      [ragtime "0.6.3"]
-      [org.postgresql/postgresql "9.4.1212"]
-      [clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"]
+      [ragtime "0.7.1"]
+      [org.postgresql/postgresql "42.1.4"]
+      [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]
       [honeysql "0.8.1"]
       [clj-time "0.12.2"]
-      [optimus "0.19.0"]]
+      [optimus "0.19.3"]]
     :plugins [[lein-environ "1.0.2"]]
     :ring
       { :handler skeleton.web/handler
@@ -45,7 +45,7 @@
           [lein-cloverage "1.0.6"]
           [lein-dotenv "RELEASE"]]
         :resource-paths ["resources" "test/resources/"]
-        :dependencies [[pjstadig/humane-test-output "0.8.1"]]
+        :dependencies [[pjstadig/humane-test-output "0.8.2"]]
         :env {:test "true" :clj-env "test"}
         :injections [(require 'pjstadig.humane-test-output)
           (pjstadig.humane-test-output/activate!)]}
@@ -54,7 +54,7 @@
         :env {:development "true" :clj-env "development"}
         :resource-paths ["resources"]
         :dependencies [[clj-livereload "0.2.0"]
-          [org.clojure/tools.nrepl "0.2.12"]]
+          [org.clojure/tools.nrepl "0.2.13"]]
         :injections [(require 'clj-livereload.server)
           (clj-livereload.server/start! {:paths ["resources/public/" "src/clj/skeleton/"] :debug? true})]
         :cooper {"test"   ["lein" "with-profile" "base,test" "test-refresh"]
@@ -63,7 +63,7 @@
       :repl
       { :plugins [[lein-dotenv "RELEASE"]]
         :env {:development "true" :clj-env "development"}
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]
         :resource-paths ["resources"]}}
     :test-refresh {:notify-command ["terminal-notifier" "-title" "Skeleton Tests" "-message"]}
     :aliases {"migrate"  ["with-profile" "base" "run" "-m" "skeleton.db.migration/migrate"]
